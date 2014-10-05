5b51f62d3f1cb7d6d69f288772d392d413c2baf6
trucedanimal@gmail.com <trucedanimal@gmail.com@bafa6843-e443-3d32-68f8-fdae76e5010e>
2014-10-05 15:36:28 +0000

    First Commit.

---
+++ SmallSet/src/ch/claude_martin/smallset/ByteConsumer.java
@@ -0,0 +1,45 @@
+package ch.claude_martin.smallset;
+
+import java.util.Objects;
+import java.util.function.IntConsumer;
+
+/**
+ * Represents an operation that accepts a single {@code byte}-valued argument and returns no result.
+ * This is the primitive type specialization of {@link Consumer} for {@code byte}. Unlike most other
+ * functional interfaces, {@code IntConsumer} is expected to operate via side-effects.
+ *
+ * @see Consumer
+ * @see IntConsumer
+ */
+@FunctionalInterface
+public interface ByteConsumer {
+
+  /**
+   * Performs this operation on the given argument.
+   *
+   * @param value
+   *          the input argument
+   */
+  void accept(byte value);
+
+  /**
+   * Returns a composed {@code ByteConsumer} that performs, in sequence, this operation followed by
+   * the {@code after} operation. If performing either operation throws an exception, it is relayed
+   * to the caller of the composed operation. If performing this operation throws an exception, the
+   * {@code after} operation will not be performed.
+   *
+   * @param after
+   *          the operation to perform after this operation
+   * @return a composed {@code ByteConsumer} that performs in sequence this operation followed by the
+   *         {@code after} operation
+   * @throws NullPointerException
+   *           if {@code after} is null
+   */
+  default ByteConsumer andThen(IntConsumer after) {
+    Objects.requireNonNull(after);
+    return (byte b) -> {
+      accept(b);
+      after.accept(b);
+    };
+  }
+}
---
+++ SmallSet/src/ch/claude_martin/smallset/SmallSetTest.java
@@ -0,0 +1,291 @@
+package ch.claude_martin.smallset;
+
+import static ch.claude_martin.smallset.SmallSetTest.Alphabet.*;
+import static ch.claude_martin.smallset.SmallSet.*;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.TreeSet;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+@SuppressWarnings("static-method")
+public class SmallSetTest {
+
+  private static final List<Byte> BAD_VALUES = asList((byte) -1, (byte) 32);
+
+  @Test
+  public final void testOf() {
+    int set = of(1, 2, 3, 31);
+    assertEquals(set, of(3, 2, 31, 2, 2, 1));
+    assertEquals(set, of((byte) 1, (byte) 2, (byte) 3, (byte) 31));
+
+    set = of(new HashSet<>(Arrays.<Number> asList(1, 2f, 3.0d, (short) 4, 5L)));
+    assertEquals(of(1, 2, 3, 4, 5), set);
+
+    for (byte i : BAD_VALUES) {
+      try {
+        of(i);
+        fail("" + i);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public final void testSingleton() {
+    assertEquals(of(2), singleton(2));
+    for (byte i : BAD_VALUES) {
+      try {
+        singleton(i);
+        fail("" + i);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public final void testEmpty() {
+    assertEquals(of(new byte[0]), empty());
+  }
+
+  @Test
+  public final void testContains() {
+    final int set = of(1, 2, 3);
+    assertTrue(contains(set, (byte) 1));
+    assertTrue(contains(set, (byte) 2));
+    assertTrue(contains(set, (byte) 3));
+    assertFalse(contains(set, (byte) 0));
+    assertFalse(contains(set, (byte) 4));
+
+    for (byte i : BAD_VALUES) {
+      try {
+        contains(0, i);
+        fail("" + i);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public final void testAdd() {
+    int set = of(1, 2, 3);
+    set = add(set, (byte) 1);
+    assertEquals(of(1, 2, 3), set);
+    set = add(set, (byte) 5);
+    assertEquals(of(1, 2, 3, 5), set);
+    set = add(set, (byte) 8);
+    assertEquals(of(1, 2, 3, 5, 8), set);
+
+    for (Byte b : BAD_VALUES) {
+      try {
+        add(set, b);
+        fail("" + b);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public final void testRemove() {
+    int set = of(1, 2, 3);
+    set = remove(set, (byte) 12);
+    assertEquals(of(1, 2, 3), set);
+    set = remove(set, (byte) 2);
+    assertEquals(of(1, 3), set);
+    set = remove(set, (byte) 2);
+    assertEquals(of(1, 3), set);
+    set = remove(set, (byte) 1);
+    assertEquals(of(3), set);
+    set = remove(set, (byte) 0);
+    assertEquals(of(3), set);
+
+    for (Byte b : BAD_VALUES) {
+      try {
+        remove(set, b);
+        fail("" + b);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public final void testIterator() {
+    final Set<Byte> set = new TreeSet<>();
+    final ByteIterator itr = iterator(of(1, 5, 10));
+    while (itr.hasNext())
+      set.add(itr.next());
+    assertEquals(set, new TreeSet<>(asList((byte) 1, (byte) 5, (byte) 10)));
+  }
+
+  @Test
+  public final void testSize() {
+    assertEquals(0, size(empty()));
+    assertEquals(1, size(singleton(0)));
+    assertEquals(1, size(singleton(5)));
+    assertEquals(4, size(of(5, 23, 4, 7)));
+  }
+
+  @Test
+  public final void testIsEmpty() {
+    assertTrue(isEmpty(empty()));
+    assertFalse(isEmpty(of(12)));
+    assertFalse(isEmpty(of(1, 2, 3)));
+  }
+
+  @Test
+  public void testOfRange() throws Exception {
+    final int set = of(5, 6, 7);
+    final int range = ofRange((byte) 5, (byte) 8);
+    assertEquals(set, range);
+
+    assertEquals(-1, ofRange((byte) 0, (byte) 32));
+
+    try {
+      ofRange(0, 0);
+      fail("0,0");
+    } catch (Exception e) {
+      // expected
+    }
+
+    for (Byte b : BAD_VALUES) {
+      try {
+        ofRange(b, 32);
+        fail("" + b);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+  }
+
+  @Test
+  public void testOfRangeClosed() throws Exception {
+    final int set = of(5, 6, 7);
+    final int range = ofRangeClosed(5, 7);
+    assertEquals(set, range);
+
+    assertEquals(-1, ofRangeClosed(0, 31));
+
+    assertEquals(of(0), ofRangeClosed(0, 0));
+
+    for (Byte b : BAD_VALUES) {
+      try {
+        ofRange(b, (byte) 32);
+        fail("" + b);
+      } catch (Exception e) {
+        // expected
+      }
+    }
+
+  }
+
+  @Test
+  public final void testToString() {
+    assertEquals("()", SmallSet.toString(empty()));
+    assertEquals("(5)", SmallSet.toString(of(5)));
+    assertEquals("(5,31)", SmallSet.toString(of(5, 31)));
+    SmallSet.toString(-1);
+  }
+
+  @Test
+  public final void testToSet() {
+
+    toSet(singleton(31));
+
+    final byte _0 = 0;
+    final byte _1 = 1;
+    final byte _5 = 5;
+    assertEquals(Collections.EMPTY_SET, toSet(empty()));
+    assertEquals(new TreeSet<>(asList(_1, _5)), toSet(of(1, 5)));
+    assertEquals(new TreeSet<>(asList(_0, _1, _5)), toSet(of(0, 1, 5)));
+    for (byte i = 0; i < 32; i++)
+      assertEquals(new TreeSet<>(asList(i)), toSet(singleton(i)));
+  }
+
+  @Test
+  public void testRandom() throws Exception {
+    final Random rng = new SecureRandom();
+    final int set = of(1, 3, 5);
+    for (int i = 0; i < 100; i++)
+      assertTrue(contains(set, random(set, rng)));
+
+    assertEquals(0, random(singleton(0), rng));
+
+  }
+
+  public static enum Alphabet {
+    A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+  }
+
+  @Test
+  public void testEnum() throws Exception {
+    int set1 = of(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
+    int set2 = of(EnumSet.allOf(Alphabet.class));
+    assertEquals(set1, set2);
+
+    EnumSet<Alphabet> enumSet = toEnumSet(set1, Alphabet.class);
+    assertEquals(EnumSet.allOf(Alphabet.class), enumSet);
+  }
+
+  @Test
+  public void testUnion() throws Exception {
+    int union = union(of(1, 2, 3), of(3, 4, 5));
+    assertEquals(ofRangeClosed(1, 5), union);
+  }
+
+  @Test
+  public void testIntersect() throws Exception {
+    int intersetion = intersect(of(1, 2, 3), of(3, 4, 5));
+    assertEquals(singleton(3), intersetion);
+  }
+
+  @Test
+  public void testComplement() throws Exception {
+    int complement = complement(of(1, 2, 3));
+    assertEquals(union(of(0), ofRange(4, 32)), complement);
+    complement = complement(of(0, 31));
+    assertEquals(ofRange(1, 31), complement);
+  }
+
+  @Test
+  public void testMinus() throws Exception {
+    int minus = minus(of(1, 2, 3, 4), of(2, 3));
+    assertEquals(of(1, 4), minus);
+  }
+
+  @Test
+  public void testIterate() throws Exception {
+    for (Byte b : iterate(of(0, 1, 3, 31))) {
+      assertTrue(b >= 0);
+    }
+  }
+
+  @Test
+  public void testForEach() throws Exception {
+    Set<Byte> set = new TreeSet<>();
+    forEach(of(1, 2, 3), set::add);
+    assertEquals(toSet(of(1, 2, 3)), set);
+  }
+
+  @Test
+  public void testStream() throws Exception {
+    for (int i = 0; i < 32; i++) {
+      assertEquals((i * (i + 1)) / 2, stream(ofRangeClosed(0, i)).sum());
+    }
+  }
+}
